6293b30be4ac0ca34e0c4f368e4bc657c6ada2b5
sahok-shustov <sashok.shustov@gmail.com>
2015-07-29 10:32:11 +0300

    Menu

--- app/src/main/java/com/example/android/astam/box_music2/Playlist.java
+++ app/src/main/java/com/example/android/astam/box_music2/Playlist.java
@@ -32,12 +32,13 @@ public class Playlist extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playlist);
 
-        jasonArray = new parseJSON("https://api.vk.com/method/audio.get?owner_id=20111260&&access_token=8b9c746a06252d374feb71641aacc858a6d902136783354f65d314a9397784556e27ff182fe4a36e55c95&album_id="+getIntent().getStringExtra("category"), "response");
+        jasonArray = new parseJSON("https://api.vk.com/method/audio.get?owner_id=20111260&&access_token=8b9c746a06252d374feb71641aacc858a6d902136783354f65d314a9397784556e27ff182fe4a36e55c95&album_id=" + getIntent().getStringExtra("category"), "response");
 
         //LinearLayout.LayoutParams imgLL = new LinearLayout.LayoutParams(85, 85);
         ImageView imageView = (ImageView) findViewById(R.id.icon);
         //imageView.setFocusable(false);
         ListView listView = (ListView) findViewById(R.id.lview);
+        listView.setFocusable(true);
         listView.setItemsCanFocus(true);
         TextView textView = (TextView) findViewById(R.id.text);
         //textView.setSelected(true);
@@ -55,7 +56,6 @@ public class Playlist extends ActionBarActivity {
                 Intent intent = new Intent(getApplicationContext(), Player.class);
                 //long songId = adapter.getItemId(position);
                 intent.putExtra("musicID", String.valueOf(position + 1));
-                intent.putExtra("musicID", String.valueOf(position+1));
                 intent.putExtra("category", getIntent().getStringExtra("category"));
                 //Log.d("999999999999999999", String.valueOf(id));
                 Log.d("55555555555555555555", String.valueOf(position));
@@ -65,7 +65,7 @@ public class Playlist extends ActionBarActivity {
 
         for (int i = 0; i < jasonArray.getJsonArray().length(); i++) {
             try {
-                idSong = jasonArray.getJsonArray().getJSONObject(i).getInt("aid");
+                //idSong = jasonArray.getJsonArray().getJSONObject(i).getInt("aid");
                 artist = jasonArray.getJsonArray().getJSONObject(i).getString("artist");
                 title = jasonArray.getJsonArray().getJSONObject(i).getString("title");
                 //pictureName = jasonArray.getJsonArray().getJSONObject(i).getString("muz_ico_name");
@@ -114,11 +114,17 @@ public class Playlist extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            /*case R.id.action_settings:
+                return true;*/
+            case R.id.backToPlayer:
+                startActivity(new Intent(this, Player.class));
+                return true;
+            case R.id.backToHomePage:
+                startActivity(new Intent(this, Box_music2.class));
+                return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 }
